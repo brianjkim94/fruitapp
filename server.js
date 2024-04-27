8e347f05a592818eed615c5e64db864fe4ab2d8b
brianjkim94 <brianjkim94@gmail.com>
2024-04-27 19:14:33 -0400

    feat: add PUT route with EDIT page

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express(); // our app
 const PORT = process.env.PORT || 3000;
+const methodOverride = require('method-override');
 
 // ------------ DATA -----------------
 // inside of fruits.js
@@ -9,10 +10,13 @@ const { veggies } = require('./models/veggies');
 const { meats } = require('./models/meats');
 
 // ------------ MIDDLEWARE ------------
+app.use(methodOverride('_method'));
 app.set('view engine', 'ejs'); // come back to this
 app.use('/', express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
+// add middleware for PUT and DELETE methods
+
 
 // ------------ ROUTES ---------------
 // ******* FRUITS INDEX ROUTE **********
@@ -35,10 +39,42 @@ app.get('/fruits/:indexOfFruitsArray', (req, res) => {
         res.render('404', {});
     } else {
         // res.send(fruits[idx]);
-        res.render('fruits/show', { fruit: fruits[idx] });
+        res.render('fruits/show', { fruit: fruits[idx], id: idx });
     }
 });
 
+// ****** GET ROUTE -- EDIT PAGE ******
+app.get('/fruits/:id/edit', (req, res) => {
+    const fruit = fruits[req.params.id];
+    let id = parseInt(req.params.id);
+    res.render('fruits/edit', { fruit, id });
+});
+
+
+// ***** POST NEW FRUIT *****
+app.post('/fruits', (req, res) => {
+    console.log('------ FORM BODY ------\n', req.body);
+    // add more code here
+    if (req.body.readyToEat === 'on') {
+        req.body.readyToEat = true;
+    } else { // req.body.readyToEat will be undefined (unchecked)
+        req.body.readyToEat = false;
+    }
+    fruits.push(req.body);
+    res.redirect('/fruits')
+});
+
+//******PUT - UPDATE FRUIT  */
+app.put('/fruits/:id', (req,res) => {
+    console.log('----UPDATE FRUIT----\n', req.body);
+    if (req.body.readyToEat === 'on') {
+        req.body.readyToEat = true;
+    } else {
+        req.body.readyToEat = false;
+    }
+    fruits[parseInt(req.params.id)] = req.body; //
+    res.redirect('/fruits'); // redirect to /fruits route to get to index page
+});
 
 // ****** VEGGIES INDEX ROUTE *******
 app.get('/veggies', (req, res) => {
@@ -87,29 +123,16 @@ app.get('/meats/:indexOfMeatsArray', (req, res) => {
 // ***** FOOD BLOG INDEX ROUTE *****
 
 app.get('/overnightoats', (req, res) => {
-    res.send('index.ejs')
+    res.render('overnightoats/index.ejs')
 });
 
 // ***** FOOD BLOG SHOW ROUTE *****
-app.get('/oohome', (req, res) => {
-    res.send('showHome.ejs')
+app.get('/overnightoats/:oohome', (req, res) => {
+    res.render('/overnightoats/showHome.ejs')
 });
 
 app.get('/ooabout', (req, res) => {
-    res.send('showAbout.ejs')
-});
-
-// ***** POST NEW FRUIT *****
-app.post('/fruits', (req, res) => {
-    console.log('------ FORM BODY ------\n', req.body);
-    // add more code here
-    if (req.body.readyToEat === 'on') {
-        req.body.readyToEat = true;
-    } else { // req.body.readyToEat will be undefined (unchecked)
-        req.body.readyToEat = false;
-    }
-    fruits.push(req.body);
-    res.redirect('/fruits')
+    res.send('/overnightoats/showAbout.ejs')
 });
 
 
